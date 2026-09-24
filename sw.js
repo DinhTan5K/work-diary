416aab9e3934919149b00ac12628857d6d2fa78c
DinhTan5K <tanv3660@gmail.com>
2026-09-25 00:15:20 +0700

    Bump version

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // =============================================
 
 // BƯỚC 1: Đổi cái này thành v để nó bắt đầu reset lại toàn bộ
-const CACHE_VERSION = 'v2.1.8'; 
+const CACHE_VERSION = 'v2.1.9'; 
 const LOGO_CACHE = 'logo-cache-v1';
 
 const APP_FILES = [
